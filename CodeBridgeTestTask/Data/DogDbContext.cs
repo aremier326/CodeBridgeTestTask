a31f428d1c77293368b03960eab9a5c2424f527d
Артем Меняк <aremier226@gmail.com>
2023-06-01 10:30:03 +0300

    Add project files.

---
+++ CodeBridgeTestTask/Data/DogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CodeBridgeTestTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeBridgeTestTask.Data
+{
+    public class DogDbContext : DbContext
+    {
+        public DogDbContext(DbContextOptions<DogDbContext> options) : base(options) { }
+
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
---
+++ CodeBridgeTestTask/Models/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeBridgeTestTask.Models
+{
+    public class Dog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Color { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int TailLength { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Weight { get; set; }
+
+    }
+}
