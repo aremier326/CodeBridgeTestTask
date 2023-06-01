9ae915ecae709bd0a2126957e2edb253a8216461
Артем Меняк <aremier226@gmail.com>
2023-06-01 11:38:39 +0300

    Added RateLimitingMiddleware and database connection.

---
+++ CodeBridgeTestTask/RateLimitingMiddleware/RateLimitingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeBridgeTestTask.RateLimitingMiddleware
+{
+    public class RateLimitingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly int _maxRequests;
+        private readonly TimeSpan _timeSpan;
+        private static readonly Queue<DateTime> RequestTimestamps = new Queue<DateTime>();
+
+        public RateLimitingMiddleware(RequestDelegate next, int maxRequests, TimeSpan timeSpan)
+        {
+            _next = next;
+            _maxRequests = maxRequests;
+            _timeSpan = timeSpan;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var now = DateTime.UtcNow;
+
+            while (RequestTimestamps.Count > 0 && now - RequestTimestamps.Peek() > _timeSpan)
+            {
+                RequestTimestamps.Dequeue();
+            }
+
+            if (RequestTimestamps.Count > _maxRequests)
+            {
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.Response.WriteAsync("Too many requests.");
+                return;
+            }
+
+            RequestTimestamps.Enqueue(now);
+            await _next(context);
+        }
+    }
+}
---
+++ CodeBridgeTestTask/RateLimitingMiddleware/RateLimitingMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeBridgeTestTask.RateLimitingMiddleware
+{
+    public static class RateLimitingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRateLimitingMiddleware(this IApplicationBuilder app,
+            int maxRequests, TimeSpan timeSpan)
+        {
+            return app.UseMiddleware<RateLimitingMiddleware>(maxRequests, timeSpan);
+        }
+    }
+}
